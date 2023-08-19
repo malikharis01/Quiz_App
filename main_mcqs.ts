34101c8988962be0901d559f65e5cd9330a1b5a1
Muhammad Haris <muhammadharisjameel6@gmail.com>
2023-08-19 09:47:03 +0500

    first commit

---
+++ main_mcqs.ts
@@ -0,0 +1,97 @@
+ import { answers } from "./questions.js"
+
+
+
+let counter = 0;
+
+if (answers.Q1 == "Islamabad") {
+
+    console.log("Correct Answer")
+counter++;
+}
+else {
+
+    console.log("Incorrect Answer and the correct answer is Islamabad")
+}
+if (answers.Q2 == "Karakoram Range") {
+
+    console.log("Correct Answer")
+counter++;
+}
+else {
+
+    console.log("Incorrect Answer and the correct answer is Karakoram Range")
+}
+if (answers.Q3 == "India") {
+    counter++;
+    console.log("Correct Answer")
+
+}
+else {
+    console.log("Incorrect Answer and the correct answer is India")
+}
+if (answers.Q4 == "Indus River") {
+
+    console.log("Correct Answer")
+counter++;
+}
+else {
+    console.log("Incorrect Answer and the correct answer is Indus River" )
+}
+if (answers.Q5 == "Sindh") {
+
+    console.log("Correct Answer")
+counter++;
+}
+else {
+    console.log("Incorrect Answer and the correct answer is Sindh")
+}
+if (answers.Q6 == "Lahore") {
+
+    console.log("Correct Answer")
+counter++;
+}
+else {
+    console.log("Incorrect Answer and the correct answer is Lahore")
+}
+if (answers.Q7 == "India") {
+
+    console.log("Correct Answer")
+counter++;
+}
+else {
+    console.log("Incorrect Answer and the correct answer is India")
+}
+if (answers.Q8 == "Allama Iqbal") {
+
+    console.log("Correct Answer")
+counter++;
+}
+else {
+    console.log("Incorrect Answer and the correct answer is Allama Iqbal")
+}
+if (answers.Q9 == "Dhol") {
+
+    console.log("Correct Answer")
+counter++;
+}
+else {
+    console.log("Incorrect Answer and the correct answer is Dhol")
+}
+if (answers.Q10 == "Karakoram Range") {
+
+    console.log("Correct Answer")
+counter++;
+}
+else {
+    console.log("Incorrect Answer and the correct answer is Karakoram Range")
+}
+console.log(`${counter} Correct`);
+let percentage = ((counter /10)*100);
+console.log(`Your score is ${percentage} %`);
+if(percentage > 40){
+    console.log("Congrats you have passed the quiz!")
+}else{
+    console.log("You are fail");
+}
+
---
+++ questions.js
@@ -0,0 +1,66 @@
+import inquirer from "inquirer";
+let numberofqs = 0;
+let answers = await inquirer.prompt([
+    {
+        name: "Q1",
+        type: "checkbox",
+        choices: ["Karachi", "Lahore", "Islamabad", "Rawalpindi"],
+        message: "What is the official name of Pakistan's capital city?",
+    },
+    {
+        name: "Q2",
+        type: "checkbox",
+        choices: ["Himalayas", "Alps", "Andes", "Karakoram Range"],
+        message: "Which mountain range is home to the world's second-highest peak, K2?",
+    },
+    {
+        name: "Q3",
+        type: "checkbox",
+        choices: ["India", "Afghanistan", "China", "Iran"],
+        message: "Pakistan shares its longest border with which neighboring country?",
+    },
+    {
+        name: "Q4",
+        type: "checkbox",
+        choices: ["Indus River", "Jhelum River", "Chenab River", "Ravi River"],
+        message: " Which river is often referred to as the 'Soul of Pakistan' and is the longest river that flows through the country?",
+    },
+    {
+        name: "Q5",
+        type: "checkbox",
+        choices: ["Punjab", "Sindh", "Balochistan", "Khyber Pakhtunkhwa"],
+        message: " The ancient city of Mohenjo-daro, part of the Indus Valley Civilization, is located in which modern-day Pakistani province?"
+    },
+    {
+        name: "Q6",
+        type: "checkbox",
+        choices: ["Karachi", "Lahore", "Islamabad", "Peshawar"],
+        message: " The Badshahi Mosque, one of the largest mosques in the world, is located in which Pakistani city?"
+    },
+    {
+        name: "Q7",
+        type: "checkbox",
+        choices: ["India", "Bangladesh", "Sri Lanka", "Nepal"],
+        message: "Pakistan was created in 1947 as a result of the partition of which British colonial territory?"
+    },
+    {
+        name: "Q8",
+        type: "checkbox",
+        choices: ["Faiz Ahmed Faiz", "Allama Iqbal", "Mirza Ghalib", "Ahmed Faraz"],
+        message: " Who is often referred to as the 'National Poet of Pakistan' and is known for his poetry in both Urdu and Persian languages?"
+    },
+    {
+        name: "Q9",
+        type: "checkbox",
+        choices: ["Sitar", "Tabla", "Dhol", "Rubabkhwa"],
+        message: "  Which traditional Pakistani musical instrument is often associated with folk music and is played using a set of wooden sticks?"
+    },
+    {
+        //         
+        name: "Q10",
+        type: "checkbox",
+        choices: ["Himalayas", "Karakoram Range", "Hindu Kush", " Pamir Mountains"],
+        message: " Which mountain range is home to some of the world's highest peaks, including K2, the second-highest mountain on Earth?"
+    },
+]);
+export { answers };
