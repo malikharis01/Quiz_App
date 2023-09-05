0d786debe42d12a23b4d2a3ebd1d2164f6b208e1
Muhammad Haris <muhammadharisjameel6@gmail.com>
2023-09-05 15:24:37 +0500

    first commit

--- main_mcqs.js
+++ main_mcqs.js
@@ -1,3 +1,4 @@
+#! /usr/bin/env node
 import { answers } from "./questions.js";
 let counter = 0;
 if (answers.Q1 == "Islamabad") {
--- main_mcqs.ts
+++ main_mcqs.ts
@@ -1,3 +1,4 @@
+#! /usr/bin/env node
  import { answers } from "./questions.js"
 
 
